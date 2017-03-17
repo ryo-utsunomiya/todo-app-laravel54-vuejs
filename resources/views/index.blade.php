a799d1218c778045981dff32d2754c93ef56d712
Ryo Utsunomiya <utsunomiya@asial.co.jp>
2017-03-17 15:47:22 +0900

    Add template

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>ToDo App with Laravel 5.4 + Vue.js</title>
+    <link rel="stylesheet" href="css/app.css"/>
+    <script type="text/javascript">
+        window.Laravel = window.Laravel || {};
+        window.Laravel.csrfToken = "{{csrf_token()}}";
+    </script>
+</head>
+<body>
+<div id="app"></div>
+<script src="js/app.js"></script>
+</body>
+</html>
